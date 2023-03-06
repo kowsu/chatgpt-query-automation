564d0d4b73397219746dbd7c06513f73b9e555d6
Ram Kowsu <ramanjineyulu.kld@gmail.com>
2023-03-06 07:05:57 +0530

    Initial commit

---
+++ src/main/java/io/chatgpt/ChatgptQueryAutomationApplication.java
@@ -0,0 +1,13 @@
+package io.chatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptQueryAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptQueryAutomationApplication.class, args);
+	}
+
+}
