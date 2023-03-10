7be3eec886a0f6c2d0e692de430547f2311e38ae
Ram Kowsu <ramanjineyulu.kld@gmail.com>
2023-03-10 16:58:52 +0530

    Printing status

--- query.py
+++ query.py
@@ -3,7 +3,7 @@ import docx
 import os
 import time
 import re
-
+import pyfiglet
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 engine="text-davinci-003"
@@ -40,32 +40,48 @@ def ask_gpt(prompt):
         answer = answer + "\n\n" + response.choices[0].text.strip()
     return answer
 
-print("Chat GPT Query automation script is running.")
+    
+ascii_banner = pyfiglet.figlet_format("Chat GPT Query Automation")
+print(ascii_banner)
+
+print("\n\nChat GPT Query automation script is running.\n\n")
 document = docx.Document()
+
+import re
 
 with open('questions.txt', 'r', encoding='utf-8') as f:
     questions = f.readlines()
+    current_chapter = ""
+    current_subchapter = ""
+    question_number = 0
     first_chapter = True 
     for i in range(len(questions)):
         if "Chapter-" in questions[i]:
             if not first_chapter:
                 document.add_page_break()
             else:
-                first_chapter = False  # unset flag for first chapter
-            document.add_heading(questions[i].strip(), level=1)
+                first_chapter = False
+            current_chapter = questions[i].strip()
+            document.add_heading(current_chapter, level=1)
+            current_subchapter = ""
+            question_number = 0
             pageBreakFlag = True
         elif "Sub Chapter:" in questions[i]:
-            document.add_heading(questions[i].strip(), level=2)
+            current_subchapter = questions[i].strip()
+            document.add_heading(current_subchapter, level=2)
+            question_number = 0
             pageBreakFlag = False
         elif "Question:" in questions[i]:
             match = re.search('\$(.*?)\$', questions[i])
             pageBreakFlag = False
             if match:
+                question_number += 1
                 question = match.group(1)
-                document.add_heading(question, level=4)
+                question_label = f"{current_chapter} - {current_subchapter} - Question {question_number} - {question}?"
+                print("Now Executing " + question_label)
+                document.add_heading(question, level=3)
                 response = ask_gpt(question)
                 document.add_paragraph(f"{response}")
-
 
 
 document.save("chat_gpt_response.docx")
